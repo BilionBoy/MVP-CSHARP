5b35d41f418fc851fe4cf5481a197ad64e4d5d0a
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-05-20 15:09:22 -0400

    refactor: adicionando lgoos e parceiros

--- obj/Debug/net8.0/MeuMVP.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuMVP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuMVP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01be1cb6e15c65d14381d0e51c5bcb526f87ed23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a88fa5cbeaf807c630caa6861ab46f46b001bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuMVP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuMVP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
